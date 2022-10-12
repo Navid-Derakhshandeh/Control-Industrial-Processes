82e6e0f64e1e846f97ef685a3ebab1955419f705
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-10-12 18:42:23 +0330

    Add files via upload

---
+++ Control Industrial Processes/Control Industrial Processes/Event.cs
@@ -0,0 +1,146 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+using System.Data.SqlClient;
+
+namespace Control_Industrial_Processes
+{
+    public partial class Event : Form
+    {
+        string connString = @"Data Source=DESKTOP-21H6HAU\DESKTOP;Initial Catalog=Industrial;Integrated Security=True;
+            Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        SqlDataAdapter dataAdapter;
+        System.Data.DataTable table;
+        SqlConnection conn;
+        string selectionStatement = "Select * from CIP";
+        public Event()
+        {
+            InitializeComponent();
+        }
+
+        private void Event_Load(object sender, EventArgs e)
+        {
+            cboSearch.SelectedIndex = 0;
+            dataGridView1.DataSource = bindingSource1;
+            GetData(selectionStatement);
+        }
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter(selectCommand, connString);
+                table = new System.Data.DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+                dataGridView1.Columns[0].ReadOnly = true;
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlCommand command;
+            string insert = @"insert into CIP(Date_Added, Issue, Des, Type)
+
+                               values(@Date_Added, @Issue, @Des, @Type)";
+            using (conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    command = new SqlCommand(insert, conn);
+                    command.Parameters.AddWithValue(@"Date_Added", dateTimePicker1.Value.Date);
+                    command.Parameters.AddWithValue(@"Issue", txtIssue.Text);
+                    command.Parameters.AddWithValue(@"Des", txtD.Text);
+                    command.Parameters.AddWithValue(@"Type", txtTI.Text);
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+
+            }
+            GetData(selectionStatement);
+            dataGridView1.Update();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            switch (cboSearch.SelectedItem.ToString())
+            {
+                case "Issue":
+                    GetData("select * from CIP where lower(issue) like '%" + txtSearch.Text.ToLower() + "%'");
+                    break;
+                case "Type":
+                    GetData("select * from CIP where lower(type) like '%" + txtSearch.Text.ToLower() + "%'");
+                    break;
+            }
+        }
+
+        private void btnExportOpen_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel._Application excel = new Microsoft.Office.Interop.Excel.Application();
+            _Workbook workbook = excel.Workbooks.Add(Type.Missing);
+            _Worksheet worksheet = null;
+            try
+            {
+                worksheet = workbook.ActiveSheet;
+                worksheet.Name = "CIP Event Log";
+                for (int rowIndex = 0; rowIndex < dataGridView1.Rows.Count - 1; rowIndex++)
+                {
+                    for (int colIndex = 0; colIndex < dataGridView1.Columns.Count; colIndex++)
+                    {
+                        if (rowIndex == 0)
+                        {
+                            worksheet.Cells[rowIndex + 1, colIndex + 1] = dataGridView1.Columns[colIndex].HeaderText;
+                        }
+                        else
+                        {
+                            worksheet.Cells[rowIndex + 1, colIndex + 1] = dataGridView1.Rows[rowIndex].Cells[colIndex].Value.ToString();
+                        }
+                    }
+
+                }
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    workbook.SaveAs(saveFileDialog1.FileName);
+                    Process.Start("excel.exe", saveFileDialog1.FileName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                excel.Quit();
+                workbook = null;
+                excel = null;
+
+            }
+        }
+    }
+}
---
+++ Control Industrial Processes/Control Industrial Processes/Temp.cs
@@ -0,0 +1,143 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace Control_Industrial_Processes
+{
+    public partial class Temp : Form
+    {
+        ModbusClient modbusClient;
+        
+        public Temp()
+        {
+            InitializeComponent();
+            //ModbusClient modbusclient = new ModbusClient("COM3");
+            //modbusClient.Connect();
+            //modbusClient.WriteSingleCoil(6, true);
+        }
+
+        private void Temp_Load(object sender, EventArgs e)
+        {
+            DateTime DT = DateTime.Now;
+            txtDT.Text = (DT.ToString("MM/dd/yyyy HH:mm:ss"));
+        }
+
+        private void aGauge1_ValueInRangeChanged(object sender, AGaugeApp.AGauge.ValueInRangeChangedEventArgs e)
+        {
+            
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            modbusClient.Disconnect();
+            LbO.Text = "Offline";
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });
+            bool[] readCoils = modbusClient.ReadCoils(9, 10);
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);
+            
+
+            aGauge1.Value = readHoldingRegisters[0];
+
+            int a = readHoldingRegisters[3];
+
+            txtTemp.Text = a.ToString();
+
+            int b = readHoldingRegisters[2];
+            LbVoltage.Text = b.ToString();
+
+            cbPower.Checked = readCoils[0];
+            cbGas.Checked = readCoils[1];
+            cbWater.Checked = readCoils[4];
+
+            string m = "High";
+            string m1 = "Low";
+            if(a >= 30)
+            {
+                Lb10.Text = m.ToString();
+                //modbusClient.WriteSingleCoil(6, true);
+            }
+            else
+            {
+                Lb10.Text = m1.ToString();
+            }
+
+            string p = "On";
+            string p1 = "Off";
+            if(cbPower.Checked == readCoils[0])
+            {
+                Lb5.Text = p.ToString();
+            }
+            else
+            {
+                Lb5.Text = p1.ToString();
+            }
+            string g = "In Flow";
+            string g1 = "";
+            if(cbGas.Checked == readCoils[1])
+            {
+                Lb6.Text = g.ToString();
+            }
+            else
+            {
+                Lb6.Text = g1.ToString();
+            }
+            string w = "In Flow";
+            string w1 = "";
+            if(cbWater.Checked == readCoils[4])
+            {
+                Lb7.Text = w.ToString();
+            }
+            else
+            {
+                Lb7.Text = w1.ToString();
+            }
+            timer1.Enabled = true;
+            string er = "Your Temperature is Very High Please Control Process";
+            string N = "No Error Found";
+            if (aGauge1.Value >= 400)
+            {
+
+                LbError.Text = er;
+            }
+            else
+            {
+                LbError.Text = N;
+            }
+            /*if(aGauge1.Value == 420)
+            {
+                MessageBox.Show("Everything Gonna be change");
+            }*/
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                modbusClient = new ModbusClient(ServerIp.Text, 502);
+                modbusClient.Connect();
+                LbO.Text = "Connected";
+                timer1.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                LbO.Text = ex.ToString();
+                throw;
+            }
+        }
+    }
+}
